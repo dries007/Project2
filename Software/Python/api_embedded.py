dc1716b123c23725f043dc850a55de2b85e38977
MichielBellekens <michiel.bellekens@hotmail.com>
2016-04-18 17:12:55 +0200

    api

--- Software/Python/api_embedded.py
+++ Software/Python/api_embedded.py
@@ -20,6 +20,8 @@ def getverinfo():
     t1.start()
     return r.text
 
+#Opvragen van een code om authenticatie te bevestigen
+# Refreshtoken moet nog worden opgeslagen in langdurige opslag --> zo lang mogelijk gebruiken
 def checkauth(devcode, clientID, interv):
     test = 1
     while (test):
@@ -37,19 +39,29 @@ def checkauth(devcode, clientID, interv):
             savevalues(data)
             test = 0
 
+# using the refresh token to get a new valid access token
+def refreshtoke(clientId, clientsecret, refreshtoken):
+    url = "https://www.googleapis.com/oauth2/v4/token"
+    grant_type="refresh_token"
+    params = {'client_id': clientId, 'client_secret': clientsecret, 'refresh_token': refreshtoken, 'grant_type': grant_type}
+    resp = requests.post(url, data=params)
+    print ("testing")
+    print(resp.text)
+
+# juiste data opvragen en filteren uit de response
 def savevalues(data):
-            acc_tok = data["access_token"]
-            tokenty = data["token_type"]
-            expire = data["expires_in"]
-            refresh = data["refresh_token"]
-            tijd = datetime.now()
-            tijd = tijd.isoformat("T") + "Z"
-            print (tijd)
-            url = "https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=" + acc_tok+"&timeMin="+tijd
-            testing = requests.get(url)
-            data = json.loads(testing.text)
-            for item in data['items']:
-                print(item['start']['dateTime'])
-                print(item['summary'])
-            #print(data['items'])
-            #print(testing.text)
+    acc_tok = data["access_token"]
+    tokenty = data["token_type"]
+    expire = data["expires_in"]
+    refresh = data["refresh_token"]
+    tijd = datetime.now()
+    tijd = tijd.isoformat("T") + "Z"
+    print (tijd)
+    url = "https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=" + acc_tok+"&timeMin="+tijd
+    testing = requests.get(url)
+    data = json.loads(testing.text)
+    for item in data['items']:
+        print(item['start']['dateTime'])
+        print(item['summary'])
+    refreshtoke("806788990556-i96frm71oem88mn63qvsudbmvhrusesf.apps.googleusercontent.com", "MFDHJWCHUl_CHAwokNvPXyR4", refresh)
+
