ee976b6caacfd20afebc5d6bc22c91fa2fbff7cc
MichielBellekens <michiel.bellekens@hotmail.com>
2016-05-13 01:12:58 +0200

    verslag en site

--- Site/about.php
+++ Site/about.php
@@ -9,7 +9,21 @@
 				<h3 class="panel-title">Panel title</h3>
 			</div>
 			<div class="panel-body">
-				Panel content
+				<p>
+					Wij zijn Dries Kennes en Michiel Bellekens, studenten ICT-Elektronica Fase 2 aan Thomas More Mechelen op Campus De Nayer. 
+				</p>
+				<p>Dit project heeft als doel het maken van een smartclock. Zoals de naam reeds doet vermoeden is het een klok/wekker met enkele slimme extra functies die nuttig kunnen zijn voor de gebruiker. 
+					Zo is de belangrijkste feature dat de gebruiker kan worden gewekt aan de hand van zijn/haar kalender. 
+				</p>
+				<p>
+					De gebruiker kan via een web interface de klok configureren naar zijn/haar eigen wensen. Ten eerste kan de gebruiker instellen hoe lang voor de eerste afspraak de wekker moet afgaan. 
+					Ten tweede kan men kiezen of men gewekt wenst te worden via een muziekfile, muziekstream of via voorgeprogrammeerde geluiden. 
+					Ten derde kan de gebruiker ook een minimum en maximum wektijd instellen. Dit betekent dat onafhankelijk van de afspraken de wekker nooit vroeger dan het minimum en later dan het maximum mag afgaan. 
+					Tot slot zijn er nog enkele instellingen voor de lay-out op het scherm zoals het formaat en de font size van de tijd en de datum.
+				</p>
+				<p>
+					Ons contacteren kan via email, <i>dries.kennes@student.thomasmore.be</i>, <i>michiel.bellekens@student.thomasmore.be</i>, of via de website, <i>dries007.net</i>.
+				</p>
 			</div>
 		</div>
 	</div>
