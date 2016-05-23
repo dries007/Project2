c08c131c72b4b7d4b3b00b570e3c0c37e5958bbe
MichielBellekens <michiel.bellekens@hotmail.com>
2016-05-24 00:19:34 +0200

    changes to site

--- Site/pictures.php
+++ Site/pictures.php
@@ -37,9 +37,9 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <img class="gridimage" src="fotos/Rotaryencoders.jpg" alt="Rotary encoders with push function">
+                <img class="gridimage" src="fotos/IMG_20160522_124839.jpg" alt="Werkende klok">
                 <div class="caption">
-                    <p class="contrasttext"> Image caption </p>
+                    <p class="contrasttext">De klok toont de dag, tijd, datum en de eerstvolgende afspraak. Indien de muziek aan het spelen is, wordt indien mogelijk de titel van het lied op de display getoond. Op de achtergrond is een deel van de web interface te zien.</p>
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@
             <div class="col-md-4">
                 <img class="gridimage" src="fotos/PCBlayout.png" alt="PCB layout">
                 <div class="caption">
-                    <p class="contrasttext">De lay-out van de PCB, gemaakt in Altium.</p>
+                    <p class="contrasttext">De lay-out van de PCB, gemaakt in Altium. Het roze gedeelte is de print voor het project. De resterende ruimte op de PCB werd opgevuld met pads voor WS2812 leds.</p>
                 </div>
             </div>
             <div class="col-md-4">
@@ -59,27 +59,7 @@
             <div class="col-md-4">
                 <img class="gridimage" src="fotos/sheet2.png" alt="Sheet  van het Altium schema.">
                 <div class="caption">
-                    <p class="contrasttext">Sheet van het Altium schema.</p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <img class="gridimage" src="fotos/pizerokit.jpg" alt="Raspberry Pi zero starter kit">
-                <div class="caption">
-                    <p class="contrasttext"> Image caption </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <img class="gridimage" src="fotos/WiFidongle.jpg" alt="WiFi dongle used">
-                <div class="caption">
-                    <p class="contrasttext"> Image caption </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <img class="gridimage" src="fotos/Rotaryencoders.jpg" alt="Rotary encoders with push function">
-                <div class="caption">
-                    <p class="contrasttext"> Image caption </p>
+                    <p class="contrasttext">Sheet 2 van het Altium schema.</p>
                 </div>
             </div>
         </div>
@@ -88,6 +68,12 @@
                 <img class="gridimage" src="fotos/ClockOn.jpg" alt="Werkende klok tijdens ontwikkeling.">
                 <div class="caption">
                     <p class="contrasttext">Werkende klok tijdens ontwikkeling.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <img class="gridimage" src="fotos/RTC.jpg" alt="RTC">
+                <div class="caption">
+                    <p class="contrasttext">Gebruikte RTC. Zeer nauwkeurig door ingebouwd kristal.</p>
                 </div>
             </div>
         </div>
